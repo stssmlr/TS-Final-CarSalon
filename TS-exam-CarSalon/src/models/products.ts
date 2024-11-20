cdc67005c85e5421c4167444b2e10fdd1e753f68
stssmlr <stanislav.smolyar@gmail.com>
2024-11-20 18:48:15 +0200

    add models

---
+++ TS-exam-CarSalon/src/models/products.ts
@@ -0,0 +1,22 @@
+export interface ProductModel {
+    id: number;
+    name: string;
+    price: number;
+    discount: number;
+    quantity: number;
+    imageUrl: string;
+    categoryName?: string;
+    description?: string;
+}
+
+export type ProductFormFields = {
+    id?: number;
+    name?: string;
+    price?: string;
+    discount?: number;
+    quantity?: number;
+    categoryId?: number;
+    description?: string;
+    imageUrl?: string;
+    image?: File;
+};
