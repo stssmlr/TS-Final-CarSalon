304a82e114a9c5bcb06c6dac9feb45cb4ce5cc5d
stssmlr <stanislav.smolyar@gmail.com>
2024-11-20 18:44:33 +0200

    Add env file

--- TS-exam-CarSalon/src/App.tsx
+++ TS-exam-CarSalon/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AppLayout from './components/Layout'
+import { Route, Routes } from 'react-router-dom'
+import ProductTable from './components/ProductTable'
+import ProductInfo from './components/ProductInfo'
+import CreateProduct from './components/CreateProduct'
+import EditProduct from './components/EditProduct'
+import Login from './components/Login'
+import Register from './components/Register'
+import { ProtectedRoute } from './security/ProtectedRoute'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path='/' element={<AppLayout />}>
+        <Route index element={<p>Home Page!</p>} />
+        <Route path='/products' element={<ProductTable />} />
+        <Route path='/products/:id' element={<ProductInfo />} />
+        <Route path='/create' element={
+          <ProtectedRoute>
+            <CreateProduct />
+          </ProtectedRoute>
+        } />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/edit/:id' element={<EditProduct />} />
+        <Route path='*' element={<p>Page Not Found!</p>} />
+      </Route>
+    </Routes>
   )
 }
 
--- TS-exam-CarSalon/src/main.tsx
+++ TS-exam-CarSalon/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AccountProvider } from './contexts/accounts.context.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AccountProvider>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </AccountProvider>,
 )
